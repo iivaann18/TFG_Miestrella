229e5cc13e9489b9bd65332d25df2eff6ca0fcbf
Alx <96348049+AlexRXWindy@users.noreply.github.com>
2025-11-07 23:48:05 +0100

    sd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,21 +1,11 @@
 /// <reference types="vite/client" />
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: any;
-    }
-  }
-}
-
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  readonly VITE_APP_URL: string
-  readonly VITE_STRIPE_PUBLISHABLE_KEY: string
+  readonly VITE_API_URL: string;
+  readonly VITE_APP_URL: string;
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
-
-export {};
